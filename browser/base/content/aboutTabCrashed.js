e7fed8b3ebacda0f2a165f7a1655568a35a855ac
Josh Porter <joporter@mozilla.com>
2026-03-27 08:13:56 -0400

    refactor: set hasReport accurately in onSetCrashReportAvailable

--- browser/base/content/aboutTabCrashed.js
+++ browser/base/content/aboutTabCrashed.js
@@ -149,7 +149,7 @@ var AboutTabCrashed = {
     let data = message.data;
 
     if (data.hasReport || data.policyAutoSubmit) {
-      this.hasReport = true;
+      this.hasReport = !data.policyAutoSubmit;
       document.documentElement.classList.add("crashDumpAvailable");
 
       document.getElementById("sendReport").checked = data.sendReport;
